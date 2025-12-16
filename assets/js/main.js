7d3bb33392f781c867c24566f5420a6c605d354b
Ghulam-Qadir <qadirr99@gmail.com>
2025-12-17 02:40:08 +0500

    final finishing fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,48 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+  
+
+  
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".mySwiper").forEach((swiperEl) => {
+      new Swiper(swiperEl, {
+        loop: true,
+        spaceBetween: 20,
+        speed: 800,
+  
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        },
+  
+        breakpoints: {
+          0: { slidesPerView: 1 },
+          576: { slidesPerView: 2 },
+          992: { slidesPerView: 3 },
+          1200: { slidesPerView: 3 },
+        },
+  
+        pagination: {
+          el: swiperEl.querySelector(".ps-arrows"),
+          clickable: true,
+        },
+  
+        navigation: {
+          nextEl: swiperEl.querySelector(".arrow.next"),
+          prevEl: swiperEl.querySelector(".arrow.prev"),
+        },
+      });
+    });
+  });
+  
+
+
+
